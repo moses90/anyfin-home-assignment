c8290ad955304e2e9c8c04ed29a490f7cf7527b9
moses90 <istvan.mozes.90@gmail.com>
2023-10-25 19:32:44 +0200

    add task5 dag

---
+++ task5__anyfin_dag.py
@@ -0,0 +1,68 @@
+from airflow import DAG
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
+
+# Define default arguments for the DAG
+default_args = {
+    'owner': 'mozes',
+    'depends_on_past': False,
+    'start_date': days_ago(1),  # Start from the previous day
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Create a DAG instance
+dag = DAG(
+    'task3_datamodel_creation',
+    default_args=default_args,
+    schedule_interval='@daily',  # Run daily
+    catchup=False,
+    max_active_runs=1,
+)
+
+# Define SQL queries for table deletion and creation
+create_encryption_extension = "create extension if not exists pgcrypto"
+delete_table_sql = "drop table if exists task5_datamodel restrict;"
+create_table_sql = """
+create table task5_datamodel as (
+	select distinct
+		c.customer_id,
+		a.email,
+		pgp_sym_encrypt(a.email,'anyfin_home_assignment') as encrypted_email
+	from cycles as c
+	left join applications as a
+	on c.customer_id = a.customer_id
+	where dpd > 10
+);
+"""
+
+# Task to create extension if doesn't exist
+delete_table_task = PostgresOperator(
+    task_id='encryption_extension',
+    postgres_conn_id='create_encryption_extension',
+    sql=delete_table_sql,
+    dag=dag,
+)
+
+# Task to delete the table if it exists
+delete_table_task = PostgresOperator(
+    task_id='delete_table',
+    postgres_conn_id='anyfin_postgres_connection',
+    sql=delete_table_sql,
+    dag=dag,
+)
+
+# Task to create a new table
+create_table_task = PostgresOperator(
+    task_id='create_table',
+    postgres_conn_id='anyfin_postgres_connection',
+    sql=create_table_sql,
+    dag=dag,
+)
+
+# Set task dependencies
+delete_table_task >> create_table_task
+
+if __name__ == "__main__":
+    dag.cli()

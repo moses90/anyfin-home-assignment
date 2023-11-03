eb71c4771fa84edd12b0d954138c82d5e4a48ae6
moses90 <istvan.mozes.90@gmail.com>
2023-11-03 14:55:22 +0100

    fix task5__anyfin_dag.py
    start date, autocommit, conn definition

--- task5__anyfin_dag.py
+++ task5__anyfin_dag.py
@@ -1,20 +1,19 @@
 from airflow import DAG
 from airflow.operators.postgres_operator import PostgresOperator
-from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
 
 # Define default arguments for the DAG
 default_args = {
     'owner': 'mozes',
     'depends_on_past': False,
-    'start_date': days_ago(1),  # Start from the previous day
+    'start_date': datetime(2023, 11, 1),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
 # Create a DAG instance
 dag = DAG(
-    'task3_datamodel_creation',
+    'task5__encryption_creation',
     default_args=default_args,
     schedule_interval='@daily',  # Run daily
     catchup=False,
@@ -22,7 +21,7 @@ dag = DAG(
 )
 
 # Define SQL queries for table deletion and creation
-create_encryption_extension = "create extension if not exists pgcrypto"
+create_encryption_extension = "create extension if not exists pgcrypto;"
 delete_table_sql = "drop table if exists task5_datamodel restrict;"
 create_table_sql = """
 create table task5_datamodel as (
@@ -38,11 +37,12 @@ create table task5_datamodel as (
 """
 
 # Task to create extension if doesn't exist
-delete_table_task = PostgresOperator(
+create_extension_task = PostgresOperator(
     task_id='encryption_extension',
-    postgres_conn_id='create_encryption_extension',
+    postgres_conn_id='anyfin_postgres_connection',
     sql=delete_table_sql,
     dag=dag,
+    autocommit=True,
 )
 
 # Task to delete the table if it exists
@@ -51,6 +51,7 @@ delete_table_task = PostgresOperator(
     postgres_conn_id='anyfin_postgres_connection',
     sql=delete_table_sql,
     dag=dag,
+    autocommit=True,
 )
 
 # Task to create a new table
@@ -59,10 +60,11 @@ create_table_task = PostgresOperator(
     postgres_conn_id='anyfin_postgres_connection',
     sql=create_table_sql,
     dag=dag,
+    autocommit=True,
 )
 
 # Set task dependencies
-delete_table_task >> create_table_task
+create_extension_task >> delete_table_task >> create_table_task
 
-if __name__ == "__main__":
-    dag.cli()
+# if __name__ == "__main__":
+#     dag.cli()
